78cfc7783815ae8b81c1b4a4ff4ea1654a11f280
sagarbhattarai609 <sagarbhattarai609@gmail.com>
2026-02-14 14:17:04 +0545

    constants

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,38 +1,41 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import Link from 'next/link';
+import {HOME_ROUTE} from "../constants/routes";
 
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full tracking-wide bg-gradient-to-br from-black via-black to-purple-700 px-8 sm:px-12 pt-16 pb-8">
       <div className="grid min-[1200px]:grid-cols-3 gap-12 xl:gap-16">
         <div className="min-[1200px]:max-w-sm max-w-lg w-full">
-          <a href='javascript:void(0)'>
+          <Link href={HOME_ROUTE}>
             <img src="https://readymadeui.com/readymadeui-light.svg" alt="logo" className="w-36" />
-          </a>
+          </Link>
           <ul className="mt-8 flex space-x-5">
             <li>
-              <a href='javascript:void(0)'>
+              <Link href='javascript:void(0)'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-600 w-8 h-8" viewBox="0 0 49.652 49.652">
                   <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" data-original="#000000" />
                 </svg>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href='javascript:void(0)'>
+              <Link href='javascript:void(0)'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 112.196 112.196">
                   <circle cx="56.098" cy="56.097" r="56.098" fill="#007ab9" data-original="#007ab9" />
                   <path fill="#fff" d="M89.616 60.611v23.128H76.207V62.161c0-5.418-1.936-9.118-6.791-9.118-3.705 0-5.906 2.491-6.878 4.903-.353.862-.444 2.059-.444 3.268v22.524h-13.41s.18-36.546 0-40.329h13.411v5.715c-.027.045-.065.089-.089.132h.089v-.132c1.782-2.742 4.96-6.662 12.085-6.662 8.822 0 15.436 5.764 15.436 18.149zm-54.96-36.642c-4.587 0-7.588 3.011-7.588 6.967 0 3.872 2.914 6.97 7.412 6.97h.087c4.677 0 7.585-3.098 7.585-6.97-.089-3.956-2.908-6.967-7.496-6.967zm-6.791 59.77H41.27v-40.33H27.865v40.33z" data-original="#f1f2f2" />
                 </svg>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href='javascript:void(0)'>
+              <Link href='javascript:void(0)'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 152 152">
                   <linearGradient id="a" x1="22.26" x2="129.74" y1="22.26" y2="129.74" gradientUnits="userSpaceOnUse">
-                    <stop offset="0" stop-color="#fae100" />
-                    <stop offset=".15" stop-color="#fcb720" />
-                    <stop offset=".3" stop-color="#ff7950" />
-                    <stop offset=".5" stop-color="#ff1c74" />
-                    <stop offset="1" stop-color="#6c1cd1" />
+                    <stop offset="0" stopColor="#fae100" />
+                    <stop offset=".15" stopColor="#fcb720" />
+                    <stop offset=".3" stopColor="#ff7950" />
+                    <stop offset=".5" stopColor="#ff1c74" />
+                    <stop offset="1" stopColor="#6c1cd1" />
                   </linearGradient>
                   <g data-name="Layer 2">
                     <g data-name="03.Instagram">
@@ -45,15 +48,15 @@ const Footer = () => {
                     </g>
                   </g>
                 </svg>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href='javascript:void(0)'>
+              <Link href='javascript:void(0)'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 1227 1227">
                   <path d="M613.5 0C274.685 0 0 274.685 0 613.5S274.685 1227 613.5 1227 1227 952.315 1227 613.5 952.315 0 613.5 0z" data-original="#000000" />
                   <path fill="#fff" d="m680.617 557.98 262.632-305.288h-62.235L652.97 517.77 470.833 252.692H260.759l275.427 400.844-275.427 320.142h62.239l240.82-279.931 192.35 279.931h210.074L680.601 557.98zM345.423 299.545h95.595l440.024 629.411h-95.595z" data-original="#ffffff" />
                 </svg>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
@@ -63,16 +66,16 @@ const Footer = () => {
             <h4 className="text-white font-medium text-base">Categories</h4>
             <ul className="mt-6 space-y-4">
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Electronics</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Electronics</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Home & Living</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Home & Living</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Men's Fashion</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Men's Fashion</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Women's Fashion</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Women's Fashion</Link>
               </li>
             </ul>
           </div>
@@ -81,16 +84,16 @@ const Footer = () => {
             <h4 className="text-white font-medium text-base">More</h4>
             <ul className="space-y-4 mt-6">
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Blog</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Blog</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Offers</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Offers</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Skincare</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Skincare</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Health & Hygiene</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Health & Hygiene</Link>
               </li>
             </ul>
           </div>
@@ -100,16 +103,16 @@ const Footer = () => {
 
             <ul className="space-y-4 mt-6">
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">About us</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">About us</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Contact Us</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Contact Us</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Suggestion</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Suggestion</Link>
               </li>
               <li>
-                <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Testinomials</a>
+                <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Testinomials</Link>
               </li>
             </ul>
           </div>
@@ -121,13 +124,13 @@ const Footer = () => {
       <div className="flex flex-wrap max-md:flex-col gap-4">
         <ul className="md:flex md:space-x-6 max-md:space-y-4">
           <li>
-            <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Terms of Service</a>
+            <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Terms of Service</Link>
           </li>
           <li>
-            <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Privacy Policy</a>
+            <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Privacy Policy</Link>
           </li>
           <li>
-            <a href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Security</a>
+            <Link href='javascript:void(0)' className="hover:text-white text-slate-400 text-sm font-normal">Security</Link>
           </li>
         </ul>
 
